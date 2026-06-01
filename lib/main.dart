d94588d40e5fef951033a4cd722d7c2ff7631bd4
ntec2000 <45743076+ntec2000@users.noreply.github.com>
2026-06-01 14:06:59 +0900

    Initial commit: 가족역사기록 도우미 v1.0.0 (22 Dart + 8528자 한자사전 + Android + GitHub Actions)

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'app.dart';
+import 'services/hanja_dict.dart';
+import 'services/db_service.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
+  // 자산 사전 / DB 사전 로딩
+  await HanjaDict.instance.load();
+  await DbService.instance.open();
+
+  runApp(const ProviderScope(child: FamilySearchHelperApp()));
+}
